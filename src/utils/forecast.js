2e25644cc3bc596c199f9c1eeb58141379bf79dd
Kevin James Stevens <kevinstevens.developer1@gmail.com>
2020-08-13 14:57:32 +0800

    Added new data: Wind speed and observation time

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,13 +5,15 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=7b256e70368faa6b70c6867aa716e0c4&query=' + longitude +','+ latitude
 
     request({ url, json: true }, (error, { body }) => {
+        console.log(body)
         if (error) {
             callback('Unable to connect to weather service', undefined)
         } else if (body.error) {
             callback('Location is invalid. Please check lat/long', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            console.log(body)
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Wind speed is ' + body.current.wind_speed + '. Observation time is ' + body.current.observation_time)
         }
     })
 }
