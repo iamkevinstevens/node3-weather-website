40c427676a8c09e4ecca8566a7099c59c246dc07
Kevin James Stevens <kevinstevens.developer1@gmail.com>
2020-08-13 14:42:07 +0800

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     formResponse.innerHTML = "Loading..."
     formResponse.classList.add("loading")
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             
             formResponse.className = "form-response"
